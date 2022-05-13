058fcdb3bd2e262a94ff4535b3a13102e8251a34
Vani-code24 <70517782+Vani-code24@users.noreply.github.com>
2022-05-13 17:31:39 +0530

    Add files via upload

--- Task1/webpack.config.js
+++ Task1/webpack.config.js
@@ -1,19 +1,18 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { appendFile } = require('fs');
+const path = require("path");
+const HTMLWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode:"development",
-    entry: {
-        bundle:path.resolve(__dirname,'src/js/index.js'),
+    entry:{
+        bundle:path.resolve(__dirname,"src/js/index.js"),
     },
     output:{
         path: path.resolve(__dirname,'dist'),
         filename:'[name].js',
     },
     devtool:'source-map',
-    devServer: {
-        static: {
+    devServer:{
+        static:{
             directory:path.resolve(__dirname,'dist')
         },
         port:3000,
@@ -22,31 +21,23 @@ module.exports = {
         compress:true,
         historyApiFallback:true,
     },
-    module :{
-        rules: [
+
+    module:{
+        rules:[
             {
-                test: /\.scss$/,
+                test:/\.scss$/,
                 use:['style-loader','css-loader','sass-loader']
-            },
-            // {
-            //     test: /\.js$/,
-            //     exclude:/node_modules/,
-            //     use:{
-            //         loader:'babel-loader',
-            //         options:{
-            //             presets:['@babel/preset-env']
-            //         }
-            //     }
-            // },
-        ],
+            }
+        ]
     },
-    plugins: [
-        new HTMLWebpackPlugin({
-            title:'web app',
-            filename:'index.html',
-            template:'src/template.html',
-            inject: 'body'
+
+    plugins:[
+        new HTMLWebPackPlugin({
+            title:"Web App",
+            filename:"index.html",
+            template:"src/template.html",
+            inject:'body',
         }),
     ],
-    
+ 
 }
